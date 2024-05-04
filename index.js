e29c9084a73b7d16b13f3d6269e4b66566121170
Fatou Fall Ndiaye <aafaatound@gmail.com>
2024-05-04 19:02:34 +0000

    last-last

--- index.js
+++ index.js
@@ -12,9 +12,33 @@ btn.onclick = function() {
         //creation de la cible
         let target = document.createElement('img');
         target.id= "target";
-        target.src="silly.png";
+        target.src="./téléchargement.png";
         container.appendChild(target);
-        target.style.top = Math.random() * (500 
+        target.style.top = Math.random() * (500 - target.offsetHeight) + 'px';
+        target.style.left = Math.random() * (600 - target.offsetWidth) + 'px';
+
+        // disparition fonction
+        setTimeout(function(){
+            target.remove();  
+        }, 2000)          
+
+        // quand on clique sur le target
+        target.onclick = function () {
+            score += 1;
+            target.style.display = 'none';
+        }
+        time-=1;
+
+        // afficher les infos
+        scoreContainer.innerHTML = `score:${score}`;
+        timeContainer.innerHTML = `time:${time}`;
+
+        //fin du jeu quand le temps est écoulé
+        if(time == 0){
+            clearInterval(interval);
+            container.innerHTML="le jeu est terminé";
+        }
+
     },1000);
 
         
